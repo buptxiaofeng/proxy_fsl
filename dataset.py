2a7650bd6ccf80cf78e543a5d40a3e6a74ff00ca
Bin Xiao <buptxiaofeng@gmail.com>
2019-12-15 19:44:23 +0800

    add num_query

--- dataset.py
+++ dataset.py
@@ -5,17 +5,18 @@ from data import MiniImagenet
 
 class MiniDataset(Dataset):
 
-    def __init__(self, data_type, num_way, num_shot, episodes):
+    def __init__(self, data_type, num_way, num_shot, num_query, episodes):
         assert data_type == "train" or data_type == "test" or data_type == "val"
 
         self.episodes = episodes
         self.num_way = num_way
         self.num_shot = num_shot
         self.data_type = data_type
+        self.num_query = num_query
         self.data = MiniImagenet()
 
-    def __getitem__(self):
-        self.load_data(self.data_type, self.num_way, self.num_shot)
+    def __getitem__(self, index):
+        return self.data.load_data(self.data_type, self.num_way, self.num_shot, self.num_query)
 
     def __len__(self):
 
