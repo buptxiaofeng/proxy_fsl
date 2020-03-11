4773af4496eaf974c7e51427b0cd9084c652ce38
Bin Xiao <buptxiaofeng@gmail.com>
2020-03-11 16:08:45 +0800

    update test acc

--- train.py
+++ train.py
@@ -89,7 +89,7 @@ def train():
                 print("episode:", epoch * parameters["num_train"] + i+1,"ce loss", total_loss / 100)
                 train_accuracy = numpy.sum(total_rewards)/1.0/parameters["num_query"] / parameters["num_way"] / parameters["num_train"]
                 print('Train Accuracy of the model on the train :{:.2f} %'.format(100 * train_accuracy))
-            if (episode % 100 == 0 and episode > 10000) or episode % 1000 == 0:
+            if (episode % 100 == 0 and episode > 5000) or episode % 1000 == 0:
                 acc, _ = evaluation(parameters, relation, val_loader, mode="val")
                 if acc > max_acc:
                     test_acc, _, = evaluation(parameters, relation, test_loader, mode="test")
