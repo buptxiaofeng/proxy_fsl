7bc3f2b6b0bb2098a985396057cecb8668bebf73
Bin Xiao <buptxiaofeng@gmail.com>
2020-04-01 14:36:17 +0800

    update resize image size

--- feat/dataloader/cub.py
+++ feat/dataloader/cub.py
@@ -36,7 +36,7 @@ class CUB(Dataset):
             ])
         else:
             self.transform = transforms.Compose([
-                transforms.Resize(int(image_size * 1.15)),
+                transforms.Resize(int(image_size * 1.1)),
                 transforms.CenterCrop(image_size),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
