9bb945ca91be2daf7df8fd1d640b5efe93842997
Bin Xiao <buptxiaofeng@gmail.com>
2020-04-01 08:37:36 +0800

    add augmentation

--- feat/dataloader/mini_imagenet.py
+++ feat/dataloader/mini_imagenet.py
@@ -4,6 +4,7 @@ from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms
 import numpy as np
+from feat.dataloader.additional_transforms import ImageJitter
 
 THIS_PATH = osp.dirname(__file__)
 ROOT_PATH = osp.abspath(osp.join(THIS_PATH, '..', '..'))
@@ -13,41 +14,24 @@ SPLIT_PATH = osp.join(ROOT_PATH, 'data/miniimagenet/split')
 class MiniImageNet(Dataset):
     """ Usage: 
     """
-    def __init__(self, setname, model_type):
-        assert model_type == "ResNet" or model_type == "ConvNet4" or model_type == "ConvNet6"
+    def __init__(self, setname, image_size, if_agumentation = False):
 
         # Transformation
-        if model_type == 'ConvNet4' or model_type == "ConvNet6":
-            image_size = 84
-            if setname == 'train':
-                self.transform = transforms.Compose([
-                    #transforms.RandomResizedCrop(image_size),
-                    transforms.Resize(92),
-                    transforms.CenterCrop(image_size),
-                    #transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4),
-                    #transforms.RandomHorizontalFlip(),
-                    transforms.ToTensor(),
-                    #Lighting(0.1, imagenet_pca['eigval'], imagenet_pca['eigvec']),
-                    transforms.Normalize(np.array([0.485, 0.456, 0.406]), np.array([0.229, 0.224, 0.225])),
-                    ])
-            else:
-                self.transform = transforms.Compose([
-                    transforms.Resize(92),
-                    #transforms.RandomResizedCrop(image_size),
-                    transforms.CenterCrop(image_size),
-                    transforms.ToTensor(),
-                    transforms.Normalize(np.array([0.485, 0.456, 0.406]), np.array([0.229, 0.224, 0.225]))
-            ])
-        elif model_type == 'ResNet':
-            #image_size = 80
+        if setname == 'train' and if_agumentation:
             self.transform = transforms.Compose([
-                transforms.Resize((224, 224)),
-                #transforms.CenterCrop(image_size),
+                transforms.RandomResizedCrop((image_size, image_size)),
+                ImageJitter(dict(Brightness=0.4, Contrast=0.4, Color=0.4)),
+                transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
-                transforms.Normalize(np.array([x / 255.0 for x in [125.3, 123.0, 113.9]]), 
-                                     np.array([x / 255.0 for x in [63.0, 62.1, 66.7]]))])
+                transforms.Normalize(np.array([0.485, 0.456, 0.406]), np.array([0.229, 0.224, 0.225])),
+                ])
         else:
-            raise ValueError('Non-supported Network Types. Please Revise Data Pre-Processing Scripts.')
+            self.transform = transforms.Compose([
+                transforms.Resize(int(image_size * 1.1)),
+                transforms.CenterCrop(image_size),
+                transforms.ToTensor(),
+                transforms.Normalize(np.array([0.485, 0.456, 0.406]), np.array([0.229, 0.224, 0.225]))
+                ])
 
         csv_path = osp.join(SPLIT_PATH, setname + '.csv')
         lines = [x.strip() for x in open(csv_path, 'r').readlines()][1:]
