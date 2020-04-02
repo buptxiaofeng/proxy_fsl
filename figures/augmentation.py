fba01849b67456503c0e77556fb39d461a299fff
Bin Xiao <buptxiaofeng@gmail.com>
2020-04-02 11:52:46 +0800

    update performance value

--- figures/augmentation.py
+++ figures/augmentation.py
@@ -25,11 +25,18 @@ if __name__ == "__main__":
     data = [[80.12, 79.41, 75.29, 76.39, 77.84, 79.34],
             [82.36, 77.24, 77.92, 82.03, 80.16, 82.02],
             [86.03, 76.15, 83.47, 85.01, 83.70, 85.17],
-            [86.03, 72.50, 84.45, 86.64, 84.05, 83.58],
-            [86.03, 71.83, 86.51, 87.86, 83.18, 84.50]]
+            [86.28, 72.50, 84.45, 86.64, 84.05, 83.58],
+            [87.05, 71.83, 86.51, 87.86, 83.18, 84.50]]
     data = pandas.DataFrame(data, index=["Conv-4", "Conv-6", "ResNet-10", "ResNet-18", "ResNet-34"], columns = ["ProxyNet_with_augmentation", "ProxyNet", "MatchingNet", "ProtoNet", "RelationNet", "Baseline++"])
+    #data = [[80.12, 79.41],
+    #        [82.36, 77.24],
+    #        [86.03, 76.15],
+    #        [86.28, 72.50],
+    #        [87.05, 71.83]]
+    #data = pandas.DataFrame(data, index=["Conv-4", "Conv-6", "ResNet-10", "ResNet-18", "ResNet-34"], columns = ["ProxyNet_with_augmentation", "ProxyNet"])
     axes = plt.gca()
     axes.set_ylim([60, 90])
     sns.lineplot(data=data)
+    plt.ylabel("Accuracy(%)")
     plt.savefig("augmentation.eps", format='eps', bbox_inches='tight')
     plt.show()
