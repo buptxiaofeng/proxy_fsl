307bc6955a175cf6144dabb2c0e3b96b146634a8
Bin Xiao <buptxiaofeng@gmail.com>
2020-05-09 12:56:26 +0800

    use wandb config

--- evaluation.py
+++ evaluation.py
@@ -16,14 +16,14 @@ def mean_confidence_interval(data, confidence=0.95):
     h = se * scipy.stats.t._ppf((1+confidence)/2., n-1)
     return m, h
 
-def evaluation(parameters, proxynet, data_loader, mode = "test"):
+def evaluation(config, proxynet, data_loader, mode = "test"):
     assert mode == "test" or mode == "val"
     num_total = 0
     if mode == "test":
-        num_total = parameters["num_way"] * parameters["num_test"] * parameters["num_query"]
+        num_total = config.num_way * config.num_test * config.num_query
 
     if mode == "val":
-        num_total = parameters["num_way"] * parameters["num_val"] * parameters["num_query"]
+        num_total = config.num_way * config.num_val * config.num_query
 
     ce = nn.CrossEntropyLoss().cuda()
 
@@ -31,13 +31,13 @@ def evaluation(parameters, proxynet, data_loader, mode = "test"):
 
     acc_list = []
 
-    label = torch.arange(parameters["num_way"]).repeat(parameters["num_query"]).cuda()
+    label = torch.arange(config.num_way).repeat(config.num_query).cuda()
     with torch.no_grad():
         total_rewards = 0
         total_loss = 0
         for batch, _ in tqdm(data_loader):
             data = batch.cuda()
-            k = parameters["num_way"] * parameters["num_shot"]
+            k = config.num_way * config.num_shot
             support, query = data[:k], data[k:]
             relation_score = proxynet(support, query)
             loss = ce(-1 * relation_score, label)
@@ -45,24 +45,9 @@ def evaluation(parameters, proxynet, data_loader, mode = "test"):
             _, predict_label = torch.min(relation_score, 1)
             rewards = [1 if predict_label[j]==label[j] else 0 for j in range(label.shape[0])]
             total_rewards += numpy.sum(rewards)
-            acc = numpy.sum(rewards) / 1.0 / parameters["num_way"] / parameters["num_query"] 
+            acc = numpy.sum(rewards) / 1.0 / config.num_way / config.num_query
             acc_list.append(acc)
 
         m, h = mean_confidence_interval(acc_list)
         print('Test mean accuracy of the model on the', mode, ' :{:.2f} %'.format(m * 100), "interval:", h * 100, "val ce loss:", total_loss / len(data_loader))
     return m, h
-
-if __name__ == "__main__":
-    json_file = open("parameters.json")
-    parameters = json.load(json_file)
-    json_file.close()
-
-    test_set = None
-    if parameters["dataset"] == "CUB":
-        test_set = CUB(setname = 'test', image_size = image_size)
-    elif parameters["dataset"] == "MiniImageNet":
-        test_set = MiniImageNet(setname = 'test', image_size = image_size)
-    save_name = str(parameters["model_type"]) + "_" + str(parameters["dataset"]) + "_" + str(parameters["num_shot"]) + "_" + str(parameters["num_way"]) + ".pth"
-    proxynet = ProxyNet(model_type = parameters["model_type"], num_shot = parameters["num_shot"], num_way = parameters["num_way"], num_query = parameters["num_query"], proxy_type = parameters["proxy_type"], classifier = parameters["classifier"]).cuda()
-    proxynet.load_state_dict(torch.load(os.path.join("weights", save_name)))
-    evaluation(parameters, proxynet, test_set)
--- train.py
+++ train.py
@@ -34,61 +34,57 @@ def init_layer(L):
 
 def train():
     wandb.init(project="proxynet_fsl")
-    json_file = open("parameters.json")
-    parameters = json.load(json_file)
-    json_file.close()
-    os.environ["CUDA_VISIBLE_DEVICES"] = parameters["gpu_id"]
+    os.environ["CUDA_VISIBLE_DEVICES"] = wandb.config.gpu_id
 
-    save_best = json.loads(parameters["save_best"].lower())
-    save_name = str(parameters["model_type"]) + "_" + str(parameters["dataset"]) + "_" + str(parameters["num_shot"]) + "_" + str(parameters["num_way"]) + ".pth"
+    save_name = str(wandb.config.model_type) + "_" + str(wandb.config.dataset) + "_" + str(wandb.config.num_shot) + "_" + str(wandb.config.num_way) + ".pth"
 
     train_set = None
     val_set = None
     test_set = None
     image_size = 84
-    if "ResNet" in parameters["model_type"]:
+    if "ResNet" in wandb.config.model_type:
         image_size = 224
-    if parameters["dataset"] == "CUB":
-        train_set = CUB(setname = "train", image_size = image_size, if_augmentation = json.loads(parameters["if_augmentation"].lower()))
+    if wandb.config.dataset == "CUB":
+        train_set = CUB(setname = "train", image_size = image_size, if_augmentation = wandb.config.if_augmentation)
         val_set = CUB(setname = 'val', image_size = image_size)
         test_set = CUB(setname = 'test', image_size = image_size)
-    elif parameters["dataset"] == "MiniImageNet":
-        train_set = MiniImageNet(setname = "train", image_size = image_size, if_augmentation = json.loads(parameters["if_augmentation"].lower()))
+    elif wandb.config.dataset == "MiniImageNet":
+        train_set = MiniImageNet(setname = "train", image_size = image_size, if_augmentation = wandb.config.if_augmentation)
         val_set = MiniImageNet(setname = 'val', image_size = image_size)
         test_set = MiniImageNet(setname = 'test', image_size = image_size)
     else:
         raise("dataset parameter value error!")
 
-    train_sampler = CategoriesSampler(train_set.label, n_batch = parameters["num_train"], n_cls = parameters["num_way"], n_per = parameters["num_shot"] + parameters["num_query"])
+    train_sampler = CategoriesSampler(train_set.label, n_batch = wandb.config.num_train, n_cls = wandb.config.num_way, n_per = wandb.config.num_shot + wandb.config.num_query)
     train_loader = DataLoader(dataset=train_set, batch_sampler = train_sampler, num_workers= 8, pin_memory = True)
-    val_sampler = CategoriesSampler(val_set.label, n_batch = parameters["num_val"], n_cls = parameters["num_way"], n_per = parameters["num_shot"] + parameters["num_query"])
+    val_sampler = CategoriesSampler(val_set.label, n_batch = wandb.config.num_val, n_cls = wandb.config.num_way, n_per = wandb.config.num_shot + wandb.config.num_query)
     val_loader = DataLoader(dataset=val_set, batch_sampler=val_sampler, num_workers=8, pin_memory=True)
-    test_sampler = CategoriesSampler(test_set.label, n_batch = parameters["num_test"], n_cls = parameters["num_way"], n_per = parameters["num_shot"] + parameters["num_query"])
+    test_sampler = CategoriesSampler(test_set.label, n_batch = wandb.config.num_test, n_cls = wandb.config.num_way, n_per = wandb.config.num_shot + wandb.config.num_query)
     test_loader = DataLoader(dataset=test_set, batch_sampler=test_sampler, num_workers=8, pin_memory=True)
 
-    proxynet = ProxyNet(model_type = parameters["model_type"], num_shot = parameters["num_shot"], num_way = parameters["num_way"], num_query = parameters["num_query"], proxy_type = parameters["proxy_type"], classifier = parameters["classifier"]).cuda()
+    proxynet = ProxyNet(model_type = wandb.config.model_type, num_shot = wandb.config.num_shot, num_way = wandb.config.num_way, num_query = wandb.config.num_query, proxy_type = wandb.config.proxy_type, classifier = wandb.config.classifier).cuda()
     wandb.watch(proxynet)
     #proxynet.apply(init_layer)
 
-    optimizer = torch.optim.SGD(proxynet.parameters(), lr = parameters["sgd_lr"])
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, "max", patience = int(parameters["patience"]), factor = float(parameters["reduce_factor"]), min_lr = 0.0001)
+    optimizer = torch.optim.SGD(proxynet.parameters(), lr = wandb.config.sgd_lr)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, "max", patience = int(wandb.config.patience), factor = float(wandb.config.reduce_factor), min_lr = 0.0001)
     ce = nn.CrossEntropyLoss().cuda()
     cudnn.benchmark = True
 
-    label = torch.arange(parameters["num_way"]).repeat(parameters["num_query"])
+    label = torch.arange(wandb.config.num_way).repeat(wandb.config.num_query)
     #one hot encode
     one_hot_label = torch.zeros(len(label), label.max()+1).scatter_(1, label.unsqueeze(1), 1.).float().cuda()
     label = label.cuda()
     max_acc = 0
     max_test_acc = 0
     train_accuracy = 0
-    for epoch in range(parameters["num_epochs"]):
+    for epoch in range(wandb.config.num_epochs):
         total_rewards = 0
         total_loss = 0
         for i, batch in enumerate(train_loader, 1):
             proxynet.train()
             data, _ = [_.cuda() for _ in batch]
-            p = parameters["num_shot"] * parameters["num_way"]
+            p = wandb.config.num_shot * wandb.config.num_way
             support, query = data[:p], data[p:]
             relation_score = proxynet(support, query)
 
@@ -102,23 +98,23 @@ def train():
             loss.backward()
             optimizer.step()
 
-            episode = epoch * parameters["num_train"] + i + 1
+            episode = epoch * wandb.config.num_train + i + 1
             if episode % 100 == 0:
-                print("episode:", epoch * parameters["num_train"] + i+1,"ce loss", total_loss / float(i + 1))
-                train_accuracy = numpy.sum(total_rewards)/1.0/parameters["num_query"] / parameters["num_way"] / parameters["num_train"]
+                print("episode:", epoch * wandb.config.num_train + i+1,"ce loss", total_loss / float(i + 1))
+                train_accuracy = numpy.sum(total_rewards)/1.0/wandb.config.num_query / wandb.config.num_way / wandb.config.num_train
                 print('Train Accuracy of the model on the train :{:.2f} %'.format(100 * train_accuracy))
             threshold = 30000
-            if parameters["dataset"] == "CUB":
+            if wandb.config.dataset == "CUB":
                 threshold = 10000
             if (episode % 100 == 0 and episode > threshold) or episode % 1000 == 0:
-                acc, _ = evaluation(parameters, proxynet, val_loader, mode="val")
+                acc, _ = evaluation(wandb.config, proxynet, val_loader, mode="val")
                 if acc > max_acc:
                     max_acc = acc
-                    test_acc, _, = evaluation(parameters, proxynet, test_loader, mode="test")
+                    test_acc, _, = evaluation(wandb.config, proxynet, test_loader, mode="test")
                     max_test_acc = test_acc
-                    if save_best:
+                    if wandb.config.save_best:
                         torch.save(proxynet.state_dict(), os.path.join("weights", save_name))
-                print("episode:", epoch * parameters["num_train"] + i+1,"max val acc:", max_acc, " max test acc:", max_test_acc)
+                print("episode:", epoch * wandb.config.num_train + i+1,"max val acc:", max_acc, " max test acc:", max_test_acc)
                 wandb.log({"val_acc": acc})
 
         scheduler.step(max_acc)
