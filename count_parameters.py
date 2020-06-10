f7680929cae434a1fd59757f7865661d8088a25e
Bin Xiao <buptxiaofeng@gmail.com>
2020-06-10 09:32:51 +0800

    add ctmnet

--- count_parameters.py
+++ count_parameters.py
@@ -1,5 +1,6 @@
 from feat.models.proxynet import ProxyNet
 from feat.models.matchnet import MatchNet
+from feat.models.ctmnet import CTMNet
 
 proxynet = ProxyNet(model_type = "ConvNet4", num_shot = 5, num_way = 5, num_query = 15, proxy_type = "Proxy", classifier = "3DConv")
 num_parameters = sum(p.numel() for p in proxynet.parameters() if p.requires_grad)
@@ -16,3 +17,7 @@ print("The number of trainable parameter: RelationNet:" + str(num_parameters))
 matchnet = MatchNet(model_type = "ConvNet4", use_bilstm = True, num_shot = 5, num_way = 5, num_query = 15)
 num_parameters = sum(p.numel() for p in matchnet.parameters() if p.requires_grad)
 print("The number of trainable parameter: MatchNet:" + str(num_parameters))
+
+ctmnet = CTMNet(model_type = "ConvNet4",  num_shot = 5, num_way = 5, num_query = 15)
+num_parameters = sum(p.numel() for p in ctmnet.parameters() if p.requires_grad)
+print("The number of trainable parameter: CTMNet:" + str(num_parameters))
